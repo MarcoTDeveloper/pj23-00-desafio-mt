219fe0b850b2dda72a5b0de17dd0815a92844afe
Marco Túlio Rodrigues da Silveira <marcosdevprofessional@gmail.com>
2023-11-06 15:09:05 -0300

    v23.3.0

--- src/services/inventory.php
+++ src/services/inventory.php
@@ -23,7 +23,7 @@ class Inventory extends API_configuration
             }
             return $inventory;
         } else {
-            return false;
+            return ['message' => 'No products in stock.'];
         }
     }
 
