1e82514a881f49aae83e3454098ba8290e6217b3
Marco Túlio Rodrigues da Silveira <marcosdevprofessional@gmail.com>
2023-11-11 19:19:10 -0300

    v23.7.0

--- src/services/inventory.php
+++ src/services/inventory.php
@@ -5,7 +5,17 @@ class Inventory extends API_configuration
 
     public function read_inventory()
     {
-        $sql = 'SELECT P.`id` AS `id`, P.`name` AS `name`, (COALESCE(`order_amount`, 0) - COALESCE(`sales_product_amount`, 0)) AS `amount`, P.`price` AS `unitary_value`, P.`price` * (COALESCE(`order_amount`, 0) - COALESCE(`sales_product_amount`, 0)) AS `total_value` FROM `products` P LEFT JOIN (SELECT `product_id`, SUM(`amount`) AS `order_amount` FROM `orders` GROUP BY `product_id`) O ON P.`id` = O.`product_id` LEFT JOIN (SELECT `product_id`, SUM(`amount`) AS `sales_product_amount` FROM `sales_products` GROUP BY `product_id`) SP ON P.`id` = SP.`product_id`;';
+        $sql = 'SELECT P.`id` AS `id`, P.`name` AS `name`, 
+        (COALESCE(`order_amount`, 0) - COALESCE(`sales_product_amount`, 0)) AS `amount`, 
+        P.`price` AS `unitary_value`, 
+        P.`price` * (COALESCE(`order_amount`, 0) - COALESCE(`sales_product_amount`, 0)) AS `total_value` 
+        FROM `products` P 
+        LEFT JOIN (SELECT `product_id`, SUM(`amount`) AS `order_amount` FROM `orders` GROUP BY `product_id`) O 
+        ON P.`id` = O.`product_id` 
+        LEFT JOIN (SELECT `product_id`, SUM(`amount`) AS `sales_product_amount` FROM `sales_products` SP
+        JOIN `sales` S ON SP.`sale_id` = S.`id`
+        WHERE S.`status` = "true"
+        GROUP BY `product_id`) SP ON P.`id` = SP.`product_id`';
         $get_inventory = $this->db_read($sql);
 
         if ($this->db_num_rows($get_inventory) > 0) {
@@ -28,7 +38,18 @@ class Inventory extends API_configuration
     public function read_stock_by_product_id(
         int $product_id
     ) {
-        $sql = 'SELECT P.`name` AS `name`, (COALESCE(`order_amount`, 0) - COALESCE(`sales_product_amount`, 0)) AS `amount` FROM `products` P LEFT JOIN (SELECT `product_id`, SUM(`amount`) AS `order_amount` FROM `orders` GROUP BY `product_id`) O ON P.`id` = O.`product_id` LEFT JOIN (SELECT `product_id`, SUM(`amount`) AS `sales_product_amount` FROM `sales_products` GROUP BY `product_id`) SP ON P.`id` = SP.`product_id` WHERE P.`id` = ' . $product_id . ';';
+        $sql = 'SELECT
+        P.`name` AS `name`,
+        (COALESCE(`order_amount`, 0) - COALESCE(`sales_product_amount`, 0)) AS `amount`
+        FROM `products` P LEFT JOIN
+        (SELECT `product_id`, SUM(`amount`) AS `order_amount` FROM `orders` GROUP BY `product_id`) O
+        ON P.`id` = O.`product_id`
+        LEFT JOIN
+        (SELECT `product_id`, SUM(`amount`) AS `sales_product_amount` FROM `sales_products` SP, `sales` S
+        WHERE SP.`sale_id` = S.`id` AND S.`status` = "true"
+        GROUP BY `product_id`) SP
+        ON P.`id` = SP.`product_id`
+        WHERE P.`id` = '. $product_id .'';
         $get_inventory = $this->db_read($sql);
         if ($this->db_num_rows($get_inventory) > 0) {
             $inventory = $this->db_object($get_inventory);
